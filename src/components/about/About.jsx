a0d02213fd2c145331726b9078ea297a5dab077b
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-05 17:35:05 +0200

    about section done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./about.css"
+import {FaAward} from "react-icons/fa"
+import {FaHotel} from "react-icons/fa"
+import {BsCodeSlash} from "react-icons/bs"
 
 const About = () => {
   return (
@@ -8,7 +11,27 @@ const About = () => {
       <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__content">
-          
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>+1 Year Working</small>
+            </article>
+            <article className="about__card">
+              <FaHotel className="about__icon"/>
+              <h5>Background</h5>
+              <small>Hosplitality</small>
+            </article>
+            <article className="about__card">
+              <BsCodeSlash className="about__icon"/>
+              <h5>Projects</h5>
+              <small>4 Projects Completed</small>
+            </article>
+          </div>
+          <p> Hello I'm Jordi</p>
+          <div className='about-button'>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+          </div>
         </div>
       </div>
     </section>
