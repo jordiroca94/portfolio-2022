a12c9de77e8414d0950246aa9217a44e2a0d676e
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-12 15:03:28 +0200

    portfolio wip

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import "./portfolio.css"
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__contai">
+        <article className='portfolio__item'>
+          <div className="portfolio__item_">
+            <h3>This is a portfolio item title</h3>
+            <a href="https://github.com/jordiroca94/Netflix" className='btn'>Github</a>
+            <a href="https://netflix-clone-2022.herokuapp.com/" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
