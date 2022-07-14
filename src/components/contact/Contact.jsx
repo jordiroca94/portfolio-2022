f245ce8df1695bba7afb13b01242ac9658a04756
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-14 18:48:51 +0200

    contact done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import "./contact.css"
 import {MdOutlineMail} from "react-icons/md"
 import {BsLinkedin} from "react-icons/bs"
 import {BsWhatsapp} from "react-icons/bs"
+import emailjs from "emailjs-com";
+
 
 const Contact = () => {
+  const form= useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_otenkoi', 'template_lm37ztw', form.current, '0qEytRhXqOf0cpUUe')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+  
   return (
     <section id="contact">
       <h5>Get in touch</h5>
@@ -12,25 +28,25 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-          <MdOutlineMail/>
+          <MdOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>jordirocasoler94@gmail.com</h5>
             <a href="mailto:jordirocasoler94@gmail.com" target="_blank">Send an email</a>
           </article>
           <article className="contact__option">
-          <BsLinkedin/>
+          <BsLinkedin className='contact__option-icon'/>
             <h4>Linkedin</h4>
             <h5>/jordi-roca-soler</h5>
             <a href="https://www.linkedin.com/in/jordi-roca-soler/" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-          <BsWhatsapp/>
+          <BsWhatsapp className='contact__option-icon'/>
             <h4>Whatsapp</h4>
             <h5>+34 622 588 996</h5>
             <a href="https://api.whatsapp.com/send?phone=34622588996" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
