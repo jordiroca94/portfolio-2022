4afc0ca17b2da164ddc2bbb6e3ad82fb3d6997b2
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-14 20:03:48 +0200

    ux ui nav improved

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,9 +3,9 @@ import "./nav.css"
 import {AiOutlineHome} from "react-icons/ai"
 import {AiOutlineUser} from "react-icons/ai"
 import {BiBook} from "react-icons/bi"
-import {RiServiceLine} from "react-icons/ri"
 import {BiMessageSquareDetail} from "react-icons/bi"
 import {useState} from "react"
+import{GrStackOverflow} from "react-icons/gr"
 
 
 
@@ -13,11 +13,26 @@ const Nav = () => {
   const [activeNav,setActiveNav] = useState("#")
   return (
     <nav>
+    <div>
       <a href="#" onClick={()=> setActiveNav("#")} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <h5>Home</h5>
+    </div>
+    <div>
       <a href="#about" onClick={()=> setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""} ><AiOutlineUser/></a>
-      <a href="#skills" onClick={()=> setActiveNav("#skills")} className={activeNav === "#skills" ? "active" : ""}><BiBook/></a>
-      <a href="#portfolio" onClick={()=> setActiveNav("#portfolio")} className={activeNav === "#portfolio" ? "active" : ""}><RiServiceLine/></a>
+      <h5>About</h5>
+    </div>
+    <div>
+      <a href="#skills" onClick={()=> setActiveNav("#skills")} className={activeNav === "#skills" ? "active" : ""}><GrStackOverflow/></a>
+      <h5>Skills</h5>
+    </div>
+    <div>
+      <a href="#portfolio" onClick={()=> setActiveNav("#portfolio")} className={activeNav === "#portfolio" ? "active" : ""}><BiBook/></a>
+      <h5>Projects</h5>
+    </div>
+    <div>
       <a href="#contact" onClick={()=> setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageSquareDetail/></a>
+      <h5>Contact</h5>
+    </div>
 
 
     </nav>
